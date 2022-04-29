cfe0ae676580bca5066bc2a9ed2754c64ba780f9
Vishruth Devan <vishruthdevan@gmail.com>
2022-04-29 23:19:35 +0530

    feat: pie chart to visualize distribution of users

--- satellite.py
+++ satellite.py
@@ -5,5 +5,15 @@ import plotly.express as px
 data = pd.read_excel('data.xlsx', sheet_name='Database')
 app = Dash()
 
+data_users = data['Users'].value_counts().reset_index(name='Counts')
+data_users.columns = ['Users', 'Counts']
+fig1 = px.pie(
+   data_users, 
+   names='Users', 
+   values="Counts", 
+   color="Counts",
+   title="Distribution of Users")
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
