d505673ff74fffdaeee6cbe82c2592ebecce4e53
Vishruth Devan <vishruthdevan@gmail.com>
2022-04-30 22:47:48 +0530

    chore: change layout of pie chart labels

--- satellite.py
+++ satellite.py
@@ -1,6 +1,8 @@
 from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import plotly.express as px
+import pycountry
+import dash_daq as daq
 
 data = pd.read_excel('data.xlsx', sheet_name='Database')
 app = Dash()
@@ -13,6 +15,13 @@ fig1 = px.pie(
    values="Counts", 
    color="Counts",
    title="Distribution of Users")
+fig1.update_traces(textposition='inside')
+fig1.update_layout(
+   uniformtext_minsize=12, 
+   uniformtext_mode='hide',
+   margin=dict(l=10, r=10, t=50, b=10),
+   paper_bgcolor="mintcream",
+   title=dict(x=0.5))
 
 
 if __name__ == '__main__':
